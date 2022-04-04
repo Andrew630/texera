3c5519a1ec8adfce92445ac8a563307d260ea7ae
Avinash Kumar <avinash0161@yahoo.com>
2022-04-04 00:40:29 -0700

    complex workflow

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/hashJoinExpensive/HashJoinExpensiveOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/hashJoinExpensive/HashJoinExpensiveOpDesc.scala
@@ -10,7 +10,22 @@ import edu.uci.ics.texera.workflow.common.operators.OperatorDescriptor
 import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, Schema}
 import edu.uci.ics.texera.workflow.operators.hashJoin.HashJoinOpDesc
 
-class HashJoinExpensiveOpDesc[K] extends HashJoinOpDesc[K] {
+class HashJoinExpensiveOpDesc[K] extends OperatorDescriptor {
+
+  @JsonProperty(required = true)
+  @JsonSchemaTitle("Small Input attr")
+  @JsonPropertyDescription("Small Input Join Key")
+  @AutofillAttributeName
+  var buildAttributeName: String = _
+
+  @JsonProperty(required = true)
+  @JsonSchemaTitle("Large input attr")
+  @JsonPropertyDescription("Large Input Join Key")
+  @AutofillAttributeNameOnPort1
+  var probeAttributeName: String = _
+
+  @JsonIgnore
+  var opExecConfig: HashJoinExpensiveOpExecConfig[K] = _
 
   override def operatorExecutor: OpExecConfig = {
     opExecConfig = new HashJoinExpensiveOpExecConfig[K](
@@ -29,4 +44,34 @@ class HashJoinExpensiveOpDesc[K] extends HashJoinOpDesc[K] {
       inputPorts = List(InputPort("small"), InputPort("large")),
       outputPorts = List(OutputPort())
     )
+
+  // remove the probe attribute in the output
+  override def getOutputSchema(schemas: Array[Schema]): Schema = {
+    Preconditions.checkArgument(schemas.length == 2)
+    val builder = Schema.newBuilder()
+    builder.add(schemas(0)).removeIfExists(probeAttributeName)
+    if (probeAttributeName.equals(buildAttributeName)) {
+      schemas(1)
+        .getAttributes()
+        .forEach(attr => {
+          if (schemas(0).containsAttribute(attr.getName()) && attr.getName() != probeAttributeName) {
+            // appending 1 to the output of Join schema in case of duplicate attributes in probe and build table
+            builder.add(new Attribute(s"${attr.getName()}#@1", attr.getType()))
+          } else {
+            builder.add(attr)
+          }
+        })
+    } else {
+      schemas(1)
+        .getAttributes()
+        .forEach(attr => {
+          if (schemas(0).containsAttribute(attr.getName())) {
+            builder.add(new Attribute(s"${attr.getName()}#@1", attr.getType()))
+          } else if (!attr.getName().equalsIgnoreCase(probeAttributeName)) {
+            builder.add(attr)
+          }
+        })
+    }
+    builder.build()
+  }
 }
