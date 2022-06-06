65e81f812f43a5a5a7680b9b2abb924d16424291
Shengquan Ni <121989255@qq.com>
2022-06-05 18:42:31 -0700

    fix scripts

--- core/scripts/server.sh
+++ core/scripts/server.sh
@@ -1,7 +1,7 @@
 cd amber
 if [ ! -z $1 ] 
 then 
-    sbt "runMain edu.uci.ics.texera.web.TexeraWebApplication $1" 
+    sbt "runMain edu.uci.ics.texera.web.TexeraWebApplication --masterIp $1" 
 else
     sbt "runMain edu.uci.ics.texera.web.TexeraWebApplication" 
 fi
--- core/scripts/worker.sh
+++ core/scripts/worker.sh
@@ -1,7 +1,7 @@
 cd amber
-if [ ! -z $1 ] 
+if [ ! -z $1 ] && [ ! -z $2 ]; 
 then 
-    sbt "runMain edu.uci.ics.texera.web.TexeraRunWorker $1" 
+    sbt "runMain edu.uci.ics.texera.web.TexeraRunWorker --masterIp $1 --workerIp $2" 
 else
     sbt "runMain edu.uci.ics.texera.web.TexeraRunWorker" 
 fi
